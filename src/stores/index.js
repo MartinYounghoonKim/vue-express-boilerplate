f105b4cbcb9e76a9dc85337e59b26eb8d3e64d33
martinyounghoonkim <hoons0131@gmail.com>
2018-03-06 23:58:43 +0900

    Add vuex entry file

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,17 +1,14 @@
 /**
- * @description Store 에 대한 entry 파일
+ * @description Vuex Store 에 대한 entry 파일
  */
+
 import Vue from 'vue';
 import Vuex from 'vuex';
 import es6Promise from 'es6-promise';
+
 es6Promise.polyfill();
-
-import { SigninStoreModule } from './signin/';
-
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  modules: {
-    signinStore: SigninStoreModule
-  }
+    // ...
 });
