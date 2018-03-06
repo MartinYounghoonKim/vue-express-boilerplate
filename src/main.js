ff260f77c0f7494130150f98e354254e4a54dfbc
martinyounghoonkim <hoons0131@gmail.com>
2018-03-07 00:03:06 +0900

    Add store entry file in vue entry file

--- src/main.js
+++ src/main.js
@@ -3,13 +3,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './stores/index';
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
+    el: '#app',
+    router,
+    store,
+    components: {
+        App
+    },
+    template: '<App/>'
+});
