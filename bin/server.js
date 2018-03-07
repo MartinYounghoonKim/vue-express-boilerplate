be521ac7c56ae1802ddd229ad1dd342bda0c3ea0
martinyounghoonkim <hoons0131@gmail.com>
2018-03-07 23:19:25 +0900

    Add server's code using for front-end server with Express server

--- bin/server.js
+++ bin/server.js
@@ -6,4 +6,15 @@ const app = express();
 const isProduction = process.env.NODE_ENV === 'production';
 const port = isProduction ? process.env.PORT : 3000;
 
+app.set('port', port);
+
 const publicPath = path.resolve(__dirname, 'dist');
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const http = require('http');
+const server = http.createServer(app);
+
+server.listen(port);
