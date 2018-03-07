aa02f8630c3789891a5b8dbffeffad29c7f9edfa
martinyounghoonkim <hoons0131@gmail.com>
2018-03-07 22:09:57 +0900

    Add Express server entry file

---
+++ bin/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const isProduction = process.env.NODE_ENV === 'production';
+const port = isProduction ? process.env.PORT : 3000;
+
+const publicPath = path.resolve(__dirname, 'dist');
