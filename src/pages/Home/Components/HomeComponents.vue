3b1328af9032189fbb69fbee4ed993dc882e80fe
martinyounghoonkim <hoons0131@gmail.com>
2018-03-07 21:33:01 +0900

    Add Home component with child component

---
+++ src/pages/Home/Components/HomeComponents.vue
@@ -0,0 +1,12 @@
+/**
+ * @description HomeComponents 에 대한 컴포넌트
+ */
+<template>
+    <!-- ...-->
+</template>
+<script>
+    // ...
+</script>
+<style>
+    /* ... */
+</style>
---
+++ src/pages/Home/Home.vue
@@ -0,0 +1,12 @@
+/**
+* @description Home 에 대한 컴포넌트
+*/
+<template>
+    <!-- ...-->
+</template>
+<script>
+    // ...
+</script>
+<style>
+    /* ... */
+</style>
