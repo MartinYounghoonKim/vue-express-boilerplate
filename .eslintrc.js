6a3750950c3fb72f0afe8cff6b3e6b787655c3e6
martinyounghoonkim <hoons0131@gmail.com>
2018-03-10 22:35:51 +0900

    Add eslint rules:

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,48 @@
-// https://eslint.org/docs/user-guide/configuring
-
 module.exports = {
-  root: true,
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  env: {
-    browser: true,
-  },
-  extends: [
-    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
-    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-    'standard'
-  ],
-  // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
-  // add your custom rules here
-  rules: {
-    // allow async-await
-    'generator-star-spacing': 'off',
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  }
+    root: true,
+    parserOptions: {
+        parser: 'babel-eslint',
+        sourceType: 'module'
+    },
+    env: {
+        browser: true,
+    },
+    extends: [
+        'plugin:vue/essential',
+        'standard'
+    ],
+    plugins: [
+        'html',
+        'standard',
+        'vue'
+    ],
+    rules: {
+        'generator-star-spacing': 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'indent': [ 'error', 4, { 'SwitchCase': 1 } ],
+        'brace-style': [ 'error', '1tbs' ],
+        'semi': [ 'error', 'always' ],
+        'no-console': 'error',
+        'comma-dangle': [ 'error', {
+            'arrays': 'never',
+            'objects': 'always-multiline',
+            'imports': 'never',
+            'exports': 'never',
+            'functions': 'never'
+        }],
+        'no-multiple-empty-lines': [ 'error', { 'max': 2, 'maxBOF': 1 }],
+        'no-undef': 'error',
+        'space-in-parens': ['error', 'never'],
+        'space-before-function-paren': [
+            'error',
+            'always'
+        ],
+        'quotes': ['error', 'single'],
+        'space-before-blocks': [
+            'error',
+            'always'
+        ],
+        'no-empty': 'error',
+        'no-duplicate-imports': 'error'
+    }
 }
