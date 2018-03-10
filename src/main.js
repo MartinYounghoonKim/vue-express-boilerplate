c6a1be21cf7761bc88e14fb7ce4f612af45d94f1
martinyounghoonkim <hoons0131@gmail.com>
2018-03-10 23:35:21 +0900

    Edit current directory's file to been fitted eslint rules

--- src/main.js
+++ src/main.js
@@ -1,19 +1,17 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import store from './stores/index';
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+// import store from './stores/index';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
     el: '#app',
     router,
-    store,
+    // store,
     components: {
-        App
+        App,
     },
-    template: '<App/>'
+    template: '<App/>',
 });
