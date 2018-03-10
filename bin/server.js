64f1d91a29d744603bbd68478057015591e17dec
martinyounghoonkim <hoons0131@gmail.com>
2018-03-10 23:35:49 +0900

    Add logic for serperating production and development

--- bin/server.js
+++ bin/server.js
@@ -13,46 +13,59 @@ const isProduction = process.env.NODE_ENV === 'production';
 const port = isProduction ? process.env.PORT : 3000;
 const publicPath = path.resolve(__dirname, 'dist');
 
-/**
- * @description webpack HMR module
- */
-const webpackDevMiddleware = require('webpack-dev-middleware');
-const webpackHotMiddleware = require('webpack-hot-middleware');
-const webpack = require('webpack');
-const webpackConfig = require('../build/dev.server.config.js');
-const compiler = webpack(webpackConfig);
+const fs = require('fs');
+app.use(express.static( "dist"));
 
-const middleware = webpackDevMiddleware(compiler, {
-    noInfo: true,
-    hot: true,
-    // publicPath: 'http://localhost:3000',
-    publicPath: '/dist/',
-    filename: 'bundle.js',
-    quiet: false,
-    lazy: false,
-    watchOptions: {
-        aggregateTimeout: 300,
-        poll: true
-    },
-    stats: {
-        colors: true,
-    },
-    historyApiFallback: true,
-});
-app.use(middleware);
-app.use(webpackHotMiddleware(compiler, {
-    log: console.log,
-    path: '/__webpack_hmr',
-    heartbeat: 10 * 1000,
-}));
-app.get('/', function(req, res) {
-    res.send('<body><div id="app"></div><script src=\'dist/bundle.js\'></script></body>');
-});
+if (!isProduction) {
+    /**
+     * @description webpack HMR module
+     */
+    const webpack = require('webpack');
+    const webpackDevMiddleware = require('webpack-dev-middleware');
+    const webpackHotMiddleware = require('webpack-hot-middleware');
+    const webpackConfig = require('../build/dev.server.config.js');
+    const compiler = webpack(webpackConfig);
+
+    const middleware = webpackDevMiddleware(compiler, {
+        noInfo: true,
+        hot: true,
+        publicPath: '/dist/',
+        filename: 'bundle.js',
+        quiet: false,
+        lazy: false,
+        watchOptions: {
+            aggregateTimeout: 300,
+            poll: true
+        },
+        stats: {
+            colors: true,
+        },
+        historyApiFallback: true,
+    });
+
+    app.use(middleware);
+    app.use(webpackHotMiddleware(compiler, {
+        log: console.log,
+        path: '/__webpack_hmr',
+        heartbeat: 10 * 1000,
+    }));
+    app.get('/', function(req, res) {
+        res.send('<body><div id="app"></div><script src=\'dist/bundle.js\'></script></body>');
+    });
+} else {
+    const template = fs.readFileSync('./dist/index.html', 'utf-8');
+
+    app.get('/', function(req, res) {
+        res.send(template);
+    });
+}
+
+
 const server = app.listen(3000, function() {
     const host = server.address().address;
     const port = server.address().port;
 
-    console.log('Example app listening at http://%s:%s', host, port);
+    console.log(`Express server has started on port:${host}:${port}`);
 });
 
 
