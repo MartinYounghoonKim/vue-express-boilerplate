a204a8ebdb6b8b208957cc8be574884b1ca510b5
martinyounghoonkim <hoons0131@gmail.com>
2018-03-11 11:17:33 +0900

    Add acceptions code for mudule hot

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 // import store from './stores/index';
-
 Vue.config.productionTip = false;
+
+if (module.hot) {
+    module.hot.accept();
+}
 
 /* eslint-disable no-new */
 new Vue({
